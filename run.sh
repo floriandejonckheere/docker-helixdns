aa1fa5adbc74294e8310688fc0e299e0c368d5c6
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-06-26 10:06:42 +0000

    Add DNS bind

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 docker run -d --name helixdns \
+	--restart=always \
+	-p 172.17.42.1:53:53/udp \
 	-v /bin/docker:/bin/docker \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	thalariond/helixdns
--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 /app/helixwatch.sh &
-helixdns -port 53 -forward 8.8.8.8:53 -etcd-address http://172.17.42.1:4001/
+
+# HelixDNS panics every 5 minutes because etcd returns empty after a timeout.
+# See https://github.com/mrwilson/helixdns/issues/15
+while true; do
+	helixdns -port 53 -forward 8.8.8.8:53 -etcd-address http://172.17.42.1:4001/
+done
